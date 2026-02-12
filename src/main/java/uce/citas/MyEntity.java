a1442a1325aee3a2329c7e99a4122df4c7c7e98f
Jhonjaxde <jjarteaga@uce.edu.ec>
2026-02-12 16:11:23 -0500

    AUTH

--- src/main/java/uce/citas/MyEntity.java
+++ src/main/java/uce/citas/MyEntity.java
@@ -27,3 +27,50 @@ import jakarta.persistence.Entity;
 public class MyEntity extends PanacheEntity {
     public String field;
 }
+
+/*
+<template>
+    <div class="login-view">
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username" required />
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required />
+        </div>
+        <button type="submit">Entrar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { getToken, setAuth } from '../clients/UsuarioClient';
+
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        async login() {
+            try {
+                const { token, expiresAt } = await getToken(this.username, this.password);
+                if (token) {
+                    setAuth(token, expiresAt);
+                    this.$router.push({ name: 'home' });
+                } else {
+                    alert('Credenciales inválidas');
+                }
+            } catch (error) {
+                alert('Credenciales inválidas');
+            }
+        }
+    }
+}
+</script>
+*/
